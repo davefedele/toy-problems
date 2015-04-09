0156543d8537ea2dcf882408b8daa56081d5ee53
David Fedele <dave.fedele@gmail.com>
2015-04-09 10:33:39 -0700

    completed

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -17,5 +17,40 @@ Output: 7 -> 0 -> 8
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2){
+    var value1 = addLinkedListValues(l1, []);
+    var value2 = addLinkedListValues(l2, []);
 
+    var result = parseInt(value1.join('')) + parseInt(value2.join(''));
+    console.log('result is ', result);
+    var resultLinkedList = numberToLinkedList(result);
+    
+    return resultLinkedList;
 };
+
+function addLinkedListValues( node, arr ){
+    arr.unshift(node.val);
+    if( node.next !== null ) {
+        addLinkedListValues(node.next, arr);   
+    }
+    return arr;
+}
+
+function numberToLinkedList( num ) {
+    var arr = num.toString().split('');
+    
+    var resultLinkedList = new ListNode(parseInt(arr[arr.length-1]));
+    arr.pop();
+    
+    var arrayToLinkedList = function( arr, node ) {
+        if( arr.length !== 0 ) {
+            console.log(arr[arr.length-1]);
+            node.next = new ListNode(parseInt(arr[arr.length-1]));
+            arr.pop();
+            arrayToLinkedList(arr, node.next);
+        }
+    };
+    
+    arrayToLinkedList(arr, resultLinkedList);
+    
+    return resultLinkedList;
+}
