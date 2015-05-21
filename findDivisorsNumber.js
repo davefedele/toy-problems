61b95e877caa17a362edfcbbef575267da59e4b7
David Fedele <dave.fedele@gmail.com>
2015-05-21 16:34:25 -0700

    solved

--- findDivisorsNumber.js
+++ findDivisorsNumber.js
@@ -4,3 +4,13 @@
 // Test.assertEquals(getDivisorsCnt(10) , 4);
 // Test.assertEquals(getDivisorsCnt(11) , 2);
 // Test.assertEquals(getDivisorsCnt(54) , 8);
+
+function getDivisorsCnt(n){
+    var count = 0;
+    for (var i=1; i<=n; i++) {
+      if (n % i === 0) {
+        count++;
+      }
+    }
+    return count;
+}
