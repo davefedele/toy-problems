d3bf910c68b5de03eb93601f1eb43b19a841bcf8
David Fedele <dave.fedele@gmail.com>
2015-05-21 16:33:46 -0700

    intial add

---
+++ findDivisorsNumber.js
@@ -0,0 +1,6 @@
+//Find the number of divisors of a positive integer n.
+
+// Test.assertEquals(getDivisorsCnt(1) , 1);
+// Test.assertEquals(getDivisorsCnt(10) , 4);
+// Test.assertEquals(getDivisorsCnt(11) , 2);
+// Test.assertEquals(getDivisorsCnt(54) , 8);
